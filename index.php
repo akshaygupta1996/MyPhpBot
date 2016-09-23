7c9b762827e9379ceb860191413bb9380ac3c052
akshaygupta1996 <gupta.akshay1996@gmail.com>
2016-09-23 21:47:35 +0530

    3.3.1

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once 'get_message.php';
+
 $access_token = "EAAZA9ubG2IzEBAEv8J3EZAmo5konmIZAC7rbcZCy2xReh1Yt6E7nmoCnzgJnZBc6U54ACtn3i6fl0y1Vm9Kj1juQHadS1chWt6EEM02YvqsD5lk1MDCKEPqrpZCbqSNAEor7xa9JEdNp1L5fJW1kr697IIhLHBXmEogq6sTxZA1dgZDZD";
 $verify_token = "planyourtour";
 $hub_verify_token = null;
@@ -12,24 +15,9 @@ if(isset($_REQUEST['hub_challenge'])) {
     echo $challenge;
 }
 
+
 $input = json_decode(file_get_contents('php://input'), true);
+GiveReply($input);
 
-$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
-$message = $input['entry'][0]['messaging'][0]['message']['text'];
 
-$data= array(
-		'recipient' => array('id'=>"$sender"),
-		'message'=>array('text'=>"$message")
-	);
-
-$options = array(
-	'http' => array(
-				'method' => 'POST',
-				'content' => json_encode($data),
-				'header'=>"Content-Type: application/json\n"
-			)
-	);
-
-$context = stream_context_create($options);
-file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$access_token",false,$context);
 ?>
