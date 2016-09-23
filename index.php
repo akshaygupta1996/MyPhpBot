9e53f9e420c93624c6da47194cf216ccae3c718f
akshaygupta1996 <gupta.akshay1996@gmail.com>
2016-09-23 20:25:40 +0530

    2.0

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-echo "Hello World";
 $access_token = "EAAZA9ubG2IzEBAEv8J3EZAmo5konmIZAC7rbcZCy2xReh1Yt6E7nmoCnzgJnZBc6U54ACtn3i6fl0y1Vm9Kj1juQHadS1chWt6EEM02YvqsD5lk1MDCKEPqrpZCbqSNAEor7xa9JEdNp1L5fJW1kr697IIhLHBXmEogq6sTxZA1dgZDZD";
 $verify_token = "fb_time_bot";
 $hub_verify_token = null;
