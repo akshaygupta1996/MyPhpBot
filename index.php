5a85e492094b4df0fb538167ef22f87764073602
akshaygupta1996 <gupta.akshay1996@gmail.com>
2016-09-23 21:31:41 +0530

    3.1

--- index.php
+++ index.php
@@ -12,12 +12,24 @@ if(isset($_REQUEST['hub_challenge'])) {
     echo $challenge;
 }
 
-
-
-
 $input = json_decode(file_get_contents('php://input'), true);
 
 $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
 
+$data= array(
+		'recipient' => array('id'=>"$sender"),
+		'message'=>array('text'=>"$message")
+	);
+
+$options = array(
+	'http' => array(
+				'method' => 'POST',
+				'content' => json_encode($data),
+				'header'=>"Content-Type: application/json\n"
+			)
+	);
+
+$context = stream_context_create($options);
+file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$access_token",false,$context);
 ?>
